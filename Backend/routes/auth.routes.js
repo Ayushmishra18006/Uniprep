fbcaaa6b285f2216903d6eb6a87cd86b939f4c51
OmPrakash23 <mishraomm5000@gmail.com>
2026-05-01 22:01:41 +0530

    created the auth controller and the auth router implementation

---
+++ Backend/routes/auth.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import {
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  getMeController,
+} from "../controllers/auth.controllers.js";
+
+const authRouter = Router();
+
+/**
+ * @route - POST /api/auth/register
+ * @description Register the user
+ * @access public
+ */
+authRouter.post("/register", registerUserController);
+
+/**
+ * @route - POST /api/auth/login
+ * @description login existing user
+ * @access public
+ */
+authRouter.post("/login", loginUserController);
+
+/**
+ * @route - GET /api/auth/logout
+ * @description Clear the token from the user's cookie and add it to blacklist model
+ * @access public
+ */
+authRouter.get("/logout", logoutUserController);
+
+/**
+ * @route - GET /api/auth/get-me
+ * @description Get the current logged-in user information
+ * @access private
+ */
+authRouter.get("/get-me", getMeController);
+
+export default authRouter;
