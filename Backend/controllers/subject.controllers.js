24b44113a17bb6bf9b5ad57eb00a23b037760ead
OmPrakash23 <mishraomm5000@gmail.com>
2026-06-06 21:52:06 +0530

    Changed the controller configs

--- Backend/controllers/subject.controllers.js
+++ Backend/controllers/subject.controllers.js
@@ -2,32 +2,24 @@ import Subject from "../models/subject.model.js";
 
 export const createSubject = async (req, res) => {
   try {
-
     const subject = await Subject.create(req.body);
 
     res.status(201).json(subject);
-
   } catch (error) {
-
     res.status(500).json({
       message: error.message,
     });
-
   }
 };
 
 export const getSubjects = async (req, res) => {
   try {
-
     const subjects = await Subject.find();
 
     res.status(200).json(subjects);
-
   } catch (error) {
-
     res.status(500).json({
       message: error.message,
     });
-
   }
 };
